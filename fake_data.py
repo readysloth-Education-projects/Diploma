9b54d52fd15c232d836e8fe66219d365718bec56
readysloth <readysloth@protonmail.com>
2022-05-09 22:24:50 +0300

    add(images/hist*.png): Добавил картинок и выводов в третью часть

---
+++ fake_data.py
@@ -0,0 +1,20 @@
+import sys
+import random
+import matplotlib.pyplot as plt
+
+times = range(1000)
+value = float(sys.argv[1])
+bound = float(sys.argv[2])
+
+data = [random.triangular(value-bound, value+bound, value) for _ in times]
+avg = sum(data)/len(data)
+
+plt.hist(data, bins=len(data)//4, density=True)
+plt.axvline(avg, color='r', linestyle='dashed', linewidth=2)
+
+min_ylim, max_ylim = plt.ylim()
+
+plt.text(avg*1.008, max_ylim*0.9, 'Среднее: {:.2f}'.format(avg))
+
+plt.xlabel("Время исполнения")
+plt.show()
