085d9cb911978ee47e5b1e3c6c576cd510612d8b
readysloth <readysloth@protonmail.com>
2022-05-19 13:51:45 +0300

    fix(images/,fake_data.py): Уменьшил гистограммы

--- fake_data.py
+++ fake_data.py
@@ -9,7 +9,7 @@ bound = float(sys.argv[2])
 data = [random.triangular(value-bound, value+bound, value) for _ in times]
 avg = sum(data)/len(data)
 
-plt.hist(data, bins=len(data)//6, density=True)
+plt.hist(data, bins=len(data)//100, density=True)
 plt.axvline(avg, color='r', linestyle='dashed', linewidth=2)
 
 min_ylim, max_ylim = plt.ylim()
@@ -17,4 +17,4 @@ min_ylim, max_ylim = plt.ylim()
 plt.text(avg*1.008, max_ylim*0.9, 'Среднее: {:.2f}'.format(avg))
 
 plt.xlabel("Время сжатия")
-plt.show()
+plt.savefig(sys.argv[3])
