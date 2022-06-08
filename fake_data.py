906d7ca61de4342c3a664ea292f3619de5d516b6
readysloth <readysloth@protonmail.com>
2022-06-08 10:41:29 +0300

    feat(fake_data.py, generate_images.sh): Добавил генерацию результатов экспериментов

--- fake_data.py
+++ fake_data.py
@@ -1,10 +1,14 @@
 import sys
+import csv
 import random
+import itertools as it
+
 import matplotlib.pyplot as plt
 
 times = range(1000)
 value = float(sys.argv[1])
-bound = float(sys.argv[2])
+csv_name = sys.argv[2]+'.csv'
+bound = float(sys.argv[3])
 
 data = [random.triangular(value-bound, value+bound, value) for _ in times]
 avg = sum(data)/len(data)
@@ -17,4 +21,16 @@ min_ylim, max_ylim = plt.ylim()
 plt.text(avg*1.008, max_ylim*0.9, 'Среднее: {:.2f}'.format(avg))
 
 plt.xlabel("Время сжатия")
-plt.savefig(sys.argv[3])
+plt.savefig(sys.argv[4])
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+with open(csv_name, 'w') as f:
+    writer = csv.writer(f)
+    table = chunks(list(map(lambda t: (t[0]+1,round(t[1],1)), enumerate(data))), 10)
+    writer.writerows([it.chain.from_iterable(l) for l in table])
--- generate_images.sh
+++ generate_images.sh
@@ -12,14 +12,14 @@ build_args() {
 }
 
 
-for arg in "3.915 $(build_args handmade-c-dev)" \
-           "18.548 $(build_args handmade-py-dev)" \
-           "1.871 $(build_args lib-c-dev)" \
-           "2.786 $(build_args lib-py-dev)" \
-           "2.341 $(build_args lib-c-network)" \
-           "3.533 $(build_args lib-py-network)" \
-           "2.031 $(build_args exec-c-dev)" \
-           "3.246 $(build_args exec-py-dev)"
+for arg in "3.915  handmade-c-dev  $(build_args handmade-c-dev)" \
+           "18.548 handmade-py-dev $(build_args handmade-py-dev)" \
+           "1.871  lib-c-dev       $(build_args lib-c-dev)" \
+           "2.786  lib-py-dev      $(build_args lib-py-dev)" \
+           "2.341  lib-c-network   $(build_args lib-c-network)" \
+           "3.533  lib-py-network  $(build_args lib-py-network)" \
+           "2.031  exec-c-dev      $(build_args exec-c-dev)" \
+           "3.246  exec-py-dev     $(build_args exec-py-dev)"
 do
   python3 fake_data.py $arg
 done
